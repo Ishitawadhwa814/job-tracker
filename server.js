7b010df2257f65cf0de24c588f0b13bc335016ac
Ishitawadhwa814 <ishitawadhwa3@gmail.com>
2026-05-15 14:29:11 +0530

    commit

--- server.js
+++ server.js
@@ -154,9 +154,15 @@ app.use('/api/resume', resumeRoutes);
 
 // app.use(express.static(buildPath));
 
+// app.use(express.static(path.join(__dirname, "dist")));
+
+// app.get("*", (req, res) => {
+//   res.sendFile(path.join(__dirname, "dist", "index.html"));
+// });
+
 app.use(express.static(path.join(__dirname, "dist")));
 
-app.get("*", (req, res) => {
+app.use((req, res) => {
   res.sendFile(path.join(__dirname, "dist", "index.html"));
 });
 
